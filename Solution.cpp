92133e88f2af61859fc9e68151d463fd71d47f5e
Zakhar <Lorventon@yandex.ru>
2024-10-07 21:30:32 +0300

    ADD main.cpp, Solution.[h/cpp]

---
+++ Solution.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Lorventon on 07.10.2024.
+//
+
+#include "Solution.h"
+
+
---
+++ Solution.h
@@ -0,0 +1,15 @@
+//
+// Created by Lorventon on 07.10.2024.
+//
+
+#ifndef TEST_2_SOLUTION_H
+#define TEST_2_SOLUTION_H
+
+class Solution
+{
+private:
+public:
+    Solution() = default;
+};
+
+#endif //TEST_2_SOLUTION_H
