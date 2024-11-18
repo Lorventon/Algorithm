205661d9d2e304bba2cd5d380bfec83a6035479e
Zakhar <Lorventon@yandex.ru>
2024-11-18 16:56:44 +0300

    Contest 3 without I and J (will be added later)

---
+++ Yandex/6_0/6.0_3.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Lorventon on 10.11.2024.
+//
---
+++ Yandex/6_0/6.0_3.h
@@ -0,0 +1,8 @@
+//
+// Created by Lorventon on 10.11.2024.
+//
+
+#ifndef TEST_2_6_0_3_H
+#define TEST_2_6_0_3_H
+
+#endif //TEST_2_6_0_3_H
